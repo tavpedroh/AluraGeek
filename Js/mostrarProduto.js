c2daff3ae82251a91cbc77ea4ccd0050ceea31bb
Pedro Henrique <pedroh.tav.p@gmail.com>
2024-11-11 14:04:33 -0300

    desenvolvimento - disposicao de cards

--- Js/mostrarProduto.js
+++ Js/mostrarProduto.js
@@ -7,10 +7,11 @@ export default function constroiCard(nome, valor, imagem){
 
     produto.className = "produtos__item";
     produto.innerHTML = `
-        <div class="produtos_card">
-            <img src="${imagem}" alt="imagem produto">
+        <div class="produto_card">
+            <img src="${imagem}" alt="imagem produto" class="produto_card_img">
             <h3 class="produto_nome">${nome}</h3>
             <p class="produto_valor">${valor}</p>
+            <button class="produto_card_button"><img src="./images/lixeira.png" class="icone_lixeira"></button>
         </div>`
 
     return produto;
